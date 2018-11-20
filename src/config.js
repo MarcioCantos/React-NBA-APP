7edbfc98d35de3c931e49f3103635c35752586fe
cantosx <cantoscorrea@gmail.com>
2018-11-20 10:15:45 -0200

    Sidenav e footer

---
+++ src/config.js
@@ -0,0 +1,7 @@
+const CURRENT_YEAR = new Date().getFullYear()
+const EXEMPLE = "ddd"
+
+export {
+  CURRENT_YEAR,
+  EXEMPLE
+}
--- src/hoc/Layout/layout.js
+++ src/hoc/Layout/layout.js
@@ -3,18 +3,31 @@ import './layout.css'
 
 //COMPONENTS
 import Header from '../../components/Header/header';
+import Footer from '../../components/Footer/footer'
 
 class Layout extends Component {
   state = {
-
+    showNav:false
   }
+
+  toggleSidenav = (action) => {
+    this.setState({
+      showNav: action
+    })
+  }
+
+
 
   render() {
     return (
       <div>
-        <Header/>
+        <Header
+          showNav={this.state.showNav} 
+          onHideNav={() => this.toggleSidenav(false)}  
+          onOpenNav={ () => this.toggleSidenav(true) }     
+        />
         {this.props.children}
-        Footer
+        <Footer />
       </div>
     );
   }
